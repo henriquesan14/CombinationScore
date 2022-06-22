8a7c5635781c99caebc6946ab5e70292cb258a55
José Henrique da Silva Santos <jose.labsit@dotzmkt.com.br>
2022-06-21 23:41:28 -0300

    estrutura inicial

---
+++ src/ScoreCombination.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ScoreCombination.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/ScoreCombination.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ScoreCombination.Core
+{
+    public class Class1
+    {
+    }
+}
